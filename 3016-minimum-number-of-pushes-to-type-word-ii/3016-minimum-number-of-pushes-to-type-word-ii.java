025393c6878a076ef375e2a788dc87e3b3aa18ed
Ganeshlokhande07 <116879482+ganesh8068@users.noreply.github.com>
2026-07-31 11:46:23 +0530

    Time: 8 ms (99.71%), Space: 48.4 MB (17.34%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+        int arr[] = new int[26];
+        for (int i = 0 ; i < n; i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            arr[index]++;
+        }
+        int minPushing = 0, pushCnt = 0;
+        Arrays.sort(arr);
+        for (int i = 25; i >= 0; i--) {
+            int currEleIdx = 25 - i;
+            if (currEleIdx % 8 == 0) pushCnt++;
+            minPushing += arr[i] * pushCnt;
+        }
+        return minPushing;
+    }
+}
